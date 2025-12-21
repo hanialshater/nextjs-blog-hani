f54c2b47b13df991dd8092aab612ec2e0e4ade18
Hani M.M. Al-Shater <hani.alshater@gmail.com>
2025-12-21 03:16:35 +0100

    fix: resolve sitemap indexing issues (404s/redirects)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { locales } from '@/i18n/config'
 
 export const dynamic = 'force-static'
 
@@ -9,15 +10,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   const blogRoutes = allBlogs
     .filter((post) => !post.draft)
-    .map((post) => ({
-      url: `${siteUrl}/blog/${post.slug}`,
-      lastModified: post.lastmod || post.date,
-    }))
+    .map((post) => {
+      const isFreeWriting = post.path.startsWith('free-writing-blog')
+      const info = {
+        url: `${siteUrl}/${post.language || 'en'}/${isFreeWriting ? 'free-writing' : 'blog'}/${
+          post.slug
+        }`,
+        lastModified: post.lastmod || post.date,
+      }
+      return info
+    })
 
-  const routes = ['', 'blog', 'free-writing', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+  const routes = ['', 'blog', 'free-writing', 'projects', 'tags'].flatMap((route) =>
+    locales.map((locale) => ({
+      url: `${siteUrl}/${locale}/${route}`.replace(/\/$/, ''), // Remove trailing slash if route is empty
+      lastModified: new Date().toISOString().split('T')[0],
+    }))
+  )
 
   return [...routes, ...blogRoutes]
 }
